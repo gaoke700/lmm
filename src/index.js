aa23f87171240278de660e1384bedf9f83ffc30e
gaoke700 <345322758@qq.com>
2017-09-29 23:40:24 +0800

    remove the system alert url_titel

--- src/index.js
+++ src/index.js
@@ -45,6 +45,16 @@
     requestAnimationFrame(yunduo);
   }
 
+  //隐藏系统alerturl
+  window.alert = function(name){
+      var iframe = document.createElement("IFRAME");
+      iframe.style.display="none";
+      iframe.setAttribute("src", 'data:text/plain,');
+      document.documentElement.appendChild(iframe);
+      window.frames[0].window.alert(name);
+      iframe.parentNode.removeChild(iframe);
+  };
+
 //预加载
   var imgArr = [
     'img/answer1-gif.gif',
